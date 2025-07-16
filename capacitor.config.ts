9c4c35b484e449b0015d14abc02fe6f6669c5e6d
Hector <hector.adjakpa@gmail.com>
2025-07-16 22:51:24 +0200

    dickerignore

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.gestion.projet',
+  appName: 'gestion-projet',
+  webDir: 'out',
+};
+
+export default config;
